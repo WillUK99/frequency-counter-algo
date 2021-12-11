f42b96b841ab1b1ca4de0aeeaace7edf9277ff42
WillUK99 <willuk1999@gmail.com>
2021-12-11 10:35:36 +0000

    initial commit -> before refactor

---
+++ app.js
@@ -0,0 +1,27 @@
+/**
+ *  Q) Write a funtion called same, which accepts two arrays. 
+ *  The function should return true if every value in the array has it's
+ *  corresponding value squared in the second array.
+ *  The frequency of values should be the same.
+ * 
+ *  Examples) 
+ *  [1, 2, 3], [9, 1, 4] //true
+ *  [1, 5], [1, 7, 2] //false
+ *  [1, 2, 1], [4, 4, 1] //false (must be the same frequency)
+ */
+
+function sameSquares(arr1, arr2) {
+    if (arr1.length !== arr2.length) return false
+    for (var i = 0; i < arr1.length; i++) {
+        let correctIndex = arr2.indexOf(arr1[i] ** 2)
+        if (correctIndex === -1) return false
+        arr2.splice(correctIndex, 1)
+    }
+    console.log(arr2)
+    return true
+}
+
+let arr1 = [1, 2, 3]
+let arr2 = [9, 1, 4]
+
+console.log(sameSquares(arr1, arr2))
