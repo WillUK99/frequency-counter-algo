b30872d861d8ec00f53b9e7bb84294c8372a8bef
WillUK99 <willuk1999@gmail.com>
2021-12-14 20:51:09 +0000

    refactored code -> no more nested loops 😎

--- app.js
+++ app.js
@@ -10,20 +10,31 @@
  *  [1, 2, 1], [4, 4, 1] //false (must be the same frequency)
  */
 
-//  O(n^2)
+
 function sameSquares(arr1, arr2) {
     if (arr1.length !== arr2.length) return false
-    for (var i = 0; i < arr1.length; i++) {
-        let correctIndex = arr2.indexOf(arr1[i] ** 2)
-        if (correctIndex === -1) return false
-        // splicing arr2 >> mutating it which is not fuego
-        arr2.splice(correctIndex, 1)
+
+    let freqCounter1 = {}
+    let freqCounter2 = {}
+
+    for (let val of arr1) {
+        freqCounter1[val] = (freqCounter1[val] || 0) + 1
     }
-    console.log(arr2)
+
+    for (let val of arr2) {
+        freqCounter2[val] = (freqCounter2[val] || 0) + 1
+    }
+    console.log(freqCounter1)
+    console.log(freqCounter2)
+    for (let key in freqCounter1) {
+        if (!(key ** 2 in freqCounter2)) return false
+        if (freqCounter2[key ** 2] !== freqCounter1[key]) return false
+    }   
     return true
 }
 
-let arr1 = [1, 2, 3]
-let arr2 = [9, 1, 4]
+let arr1 = [1, 2, 3], arr2 = [1, 4, 9]
+// let arr1 = [1, 1, 4], arr2 = [1, 16, 16]
+// let arr1 = [1, 2, 4], arr2 = [1, 6]
 
 console.log(sameSquares(arr1, arr2))
